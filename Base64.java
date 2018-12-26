9ce4689b08024b3facf1167031f007ac98f3a65a
AliRezaBeigy <46004092+AliRezaBeigy@users.noreply.github.com>
2018-12-26 11:11:19 +0330

    add codes

---
+++ Base64.java
@@ -0,0 +1,15 @@
+package xorcrypto;
+
+public class Base64 {
+    public static String encrypt(String plainText) {
+        String result = plainText;
+        // Compelet it
+        return result;
+    }
+
+    public static String decrypt(String base64) {
+        String result = base64;
+        // Compelet it
+        return result;
+    }
+}
---
+++ XorCrypto.java
@@ -0,0 +1,46 @@
+package xorcrypto;
+
+public class XorCrypto {
+
+    static final String KEY = "Password";
+    static final String SEPARATOR = "----------------------------------------";
+    static String[] tests = new String[]{"Hello", "This is a test", "I want to encode me then decode me :D"};
+
+    public static void main(String[] args) {
+        for (String test : tests) {
+            String encoded = encrypt(test, KEY);
+            String decrypted = decrypt(encoded, KEY);
+            System.out.printf("%s%n%s -> %s%n%s -> %s%n", SEPARATOR, test, encoded, encoded, decrypted);
+        }
+    }
+
+    static String encrypt(String plainText, String key) {
+        String result = xor(plainText, key);
+        return Base64.encrypt(result);
+    }
+
+    static String decrypt(String base64, String key) {
+        String result = xor(Base64.decrypt(base64), key);
+        return result;
+    }
+
+    static String xor(String input, String key) {
+        String result = input;
+        // Compelet it
+        // help : r = (char)(i ^ k);
+        return result;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //                                             Output                                                   //
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //      Hello -> GAQfHxg=                                                                               //
+    //      GAQfHxg= -> Hello                                                                               //
+    //      ----------------------------------------                                                        //
+    //      This is a test -> BAkaAFcGAUQxQQcWBBs=                                                          //
+    //      BAkaAFcGAUQxQQcWBBs= -> This is a test                                                          //
+    //      ----------------------------------------                                                        //
+    //      I want to encode me then decode me :D -> GUEEEhkbUhA/QRYdFAAWAXAMFlMDBxcKcAUWEBgLF0Q9BFNJMw==   //
+    //      GUEEEhkbUhA/QRYdFAAWAXAMFlMDBxcKcAUWEBgLF0Q9BFNJMw== -> I want to encode me then decode me :D   //
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+}
