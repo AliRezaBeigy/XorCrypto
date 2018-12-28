4cd20ba3cddc4677309e85facde5ec0d3b247555
bardia47 <46025237+bardia47@users.noreply.github.com>
2018-12-28 15:50:04 +0330

    Update Base64.java

--- Base64.java
+++ Base64.java
@@ -3,13 +3,14 @@ package xorcrypto;
 public class Base64 {
     public static String encrypt(String plainText) {
         String result = plainText;
-        // Compelet it
+       result=Base64.getEncoder().encodeToString(result.getBytes());
         return result;
     }
 
     public static String decrypt(String base64) {
         String result = base64;
-        // Compelet it
+        byte b[]=Base64.getDecoder().decode(result);
+        result=new String(b);
         return result;
     }
 }
